f0eb0a0e0d64eb9da347157a13d901e8382618be
Christoph Bratschi <bratschi@appamics.com>
2020-08-10 18:03:46 +0200

    more

--- src/base.cpp
+++ src/base.cpp
@@ -486,10 +486,9 @@ void AminoGfx::measureRenderingEnd() {
 
         //optional output on screen (color: blue)
         if (propShowFPS->value) {
-            //cbxx TODO verify
             printf("\033[0;34m");
             printf("%i FPS (max: %i ms, min: %i ms, avg: %i ms)\n", (int)lastFPS, (int)fpsCycleMax, (int)fpsCycleMin, (int)lastCycleAvg);
-            printf("\033[0m;");
+            printf("\033[0m");
         }
     }
 }
